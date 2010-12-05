c0671a7ea21c17a80a76e2c00568f2963c3b6be6
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2010-12-05 21:54:04 +0100

    [Branch jrmol][Git] Initialised the Git repository.

--- rmol/rmol/service/sources.mk
+++
@@ -1,7 +0,0 @@
-rmol_svc_h_sources = \
-		$(top_srcdir)/rmol/service/ServiceAbstract.hpp \
-		$(top_srcdir)/rmol/service/RMOL_ServiceContext.hpp
-rmol_svc_cc_sources = \
-		$(top_srcdir)/rmol/service/ServiceAbstract.cpp \
-		$(top_srcdir)/rmol/service/RMOL_ServiceContext.cpp \
-		$(top_srcdir)/rmol/service/RMOL_Service.cpp
